afd0ad9a9af34ef1d1641a71ccf1baed555a1a72
Sam Deane <sam@elegantchaos.com>
2022-07-11 16:57:12 +0100

    Write default config options to temp directory.

--- Plugins/SwiftFormatterPlugin/SwiftFormatterPlugin.swift
+++ Plugins/SwiftFormatterPlugin/SwiftFormatterPlugin.swift
@@ -13,14 +13,21 @@ struct FormatterPlugin: CommandPlugin {
         let swiftFormatTool = try context.tool(named: "swiftformat")
         let swiftFormatExec = URL(fileURLWithPath: swiftFormatTool.path.string)
         
-        var baseArguments: [String] = []
+        let cacheFile = context.pluginWorkDirectory.appending("swift-format-cache").string
+        var baseArguments: [String] = ["--quiet", "--cache", cacheFile]
 
         let configFile = context.package.directory.appending(".swiftformat").string
-        if !FileManager.default.fileExists(atPath: configFile), let defaultPath = defaultConfigPath {
+        if !FileManager.default.fileExists(atPath: configFile), let defaultPath = defaultConfigPath(context: context) {
             baseArguments.append(contentsOf: ["--config", defaultPath])
         }
 
-
+        let swiftVersionFile = context.package.directory.appending(".swift-version").string
+        if !FileManager.default.fileExists(atPath: swiftVersionFile) {
+            let tools = context.package.toolsVersion
+            let version = "\(tools.major).\(tools.minor)"
+            baseArguments.append(contentsOf: ["--swiftversion", version])
+        }
+        
         for target in context.package.targets {
             guard let target = target as? SourceModuleTarget else { continue }
 
@@ -34,7 +41,7 @@ struct FormatterPlugin: CommandPlugin {
             process.waitUntilExit()
             
             if process.terminationReason == .exit && process.terminationStatus == 0 {
-                print("Formatted the source code in \(target.directory).")
+                Diagnostics.remark("Formatted the source code in \(target.directory).")
             } else {
                 let problem = "\(process.terminationReason):\(process.terminationStatus)"
                 Diagnostics.error("swift-format invocation failed: \(problem)")
@@ -42,23 +49,27 @@ struct FormatterPlugin: CommandPlugin {
         }
     }
     
-    var defaultConfigPath: String? {
+    func defaultConfigPath(context: PluginContext) -> String? {
         let path = ("~/.swiftformat" as NSString).expandingTildeInPath
-        if !FileManager.default.fileExists(atPath: path) {
-            let defaultConfig = """
-                # Place your default swift-format configuration options here.
-                # See https://github.com/nicklockwood/SwiftFormat#config-file for more details.
-
-                """
-            
-            do {
-                try defaultConfig.write(toFile: path, atomically: true, encoding: .utf8)
-            } catch {
-                print(error)
-                return nil
-            }
+        if FileManager.default.fileExists(atPath: path) {
+            return path
         }
+        
+        let defaultPath = context.pluginWorkDirectory.appending("default-swift-format-config").string
+        let defaultConfig = """
+            # Default Elegant Chaos swift-format configuration options.
+            # See https://github.com/nicklockwood/SwiftFormat#config-file for more details.
+            # To override these, place a .swiftformat file into your source directory, or at ~/.swiftformat.
 
-        return path
+            --ifdef no-indent
+            """
+            
+        do {
+            try defaultConfig.write(toFile: defaultPath, atomically: true, encoding: .utf8)
+            return defaultPath
+        } catch {
+            print(error)
+            return nil
+        }
     }
 }
